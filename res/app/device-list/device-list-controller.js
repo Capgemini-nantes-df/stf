ccaac594dce9383324653e7a9d3cc433a533ae31
Thomas MENARD <thomas.menard@capgemini.com>
2018-04-19 15:05:33 +0200

    Capture d'écran sur plusieurs devices.

--- res/app/device-list/device-list-controller.js
+++ res/app/device-list/device-list-controller.js
@@ -8,11 +8,16 @@ module.exports = function DeviceListCtrl(
 , ControlService
 , SettingsService
 , $location
+, $rootScope
 ) {
+
   $scope.tracker = DeviceService.trackAll($scope)
   $scope.control = ControlService.create($scope.tracker.devices, '*ALL')
 
   $scope.columnDefinitions = DeviceColumnService
+  $scope.screenshotsAll = []
+  $scope.blurUrl = false
+  var faviconIsSet = false
 
   var defaultColumns = [
     {
@@ -65,10 +70,6 @@ module.exports = function DeviceListCtrl(
     }
   , {
       name: 'cpuPlatform'
-    , selected: false
-    }
-  , {
-      name: 'openGLESVersion'
     , selected: false
     }
   , {
@@ -180,6 +181,12 @@ module.exports = function DeviceListCtrl(
     })
   }
 
+  $scope.kick = function(device) {
+    return GroupService.kick(device).then(function() {
+      $scope.$digest()
+    })
+  }
+
   $scope.applyFilter = function(query) {
     $scope.filter = QueryParser.parse(query)
   }
@@ -201,4 +208,119 @@ module.exports = function DeviceListCtrl(
     $scope.sort = defaultSort
     $scope.columns = defaultColumns
   }
+
+/********************************************************************/
+/****Ajout de fonctionnalités - tous les appareils qu'on utilise****/
+/******************************************************************/
+
+ /**
+ * Sélection de tous les appareils utilisés en meme temps.
+ */
+  $scope.selectAll = function()
+  {
+    for(var i=0; i < $scope.tracker.devices.length; i++)
+    {
+      $scope.invite($scope.tracker.devices[i])
+    }    
+  }
+
+  /**
+  * Déselection de tous les appareils utilisés en meme temps.
+  */
+  $scope.deselectAll = function()
+  {
+    for(var i=0; i < $scope.tracker.devices.length; i++)
+    {
+      $scope.kick($scope.tracker.devices[i])
+    }    
+  }
+
+
+ /** 
+ * Réalisation de screenshots sur tous les appareils utilisés en meme temps
+ */
+  $scope.takeScreenshotAll = function()
+  {
+    $scope.screenshotsAll = []
+
+    for(var i=0; i < $scope.tracker.devices.length; i++)
+    {
+      if($scope.tracker.devices[i].state == 'using')
+      {
+            $scope.control = ControlService.create($scope.tracker.devices[i], $scope.tracker.devices[i].channel)
+
+            $scope.control.screenshot().then(function(result) 
+            {
+                $scope.$apply(function() 
+                {
+                  $scope.screenshotsAll.unshift(result)
+                })
+            })
+      }   
+    }  
+  }
+  /**
+  * Nettoyage des screenshots
+  */
+  $scope.clearAll = function() 
+  {
+    $scope.screenshotsAll = []
+  }
+
+
+
+ /**
+  * Ouverture du navigateur à l'url voulu sur tous les appareils utilisés en meme temps
+  */
+  $scope.openBrowserAll=function(url)
+  {
+    for(var i=0; i < $scope.tracker.devices.length; i++)
+    {
+      if($scope.tracker.devices[i].state=='using')
+      {
+
+        $scope.control = ControlService.create($scope.tracker.devices[i], $scope.tracker.devices[i].channel)
+        $scope.control.openBrowser(url,$scope.tracker.devices[i].browser.apps[0])
+      }      
+    }    
+  }
+  $scope.openURLAll = function() 
+  {
+    $scope.blurUrl = true
+    $rootScope.screenFocus = true
+
+    //Appel de la fonction de formation de l'url
+    var url = addHttpAll($scope.textURL)
+    
+    //Appel de la fonction de modification du favicon du site
+    setUrlFaviconAll(url)
+
+    return $scope.openBrowserAll(url)
+  }
+
+  /**
+  * Formation de l'url
+  */
+  function addHttpAll(textUrl) 
+  {
+    // Check for '://' because a protocol-less URL might include
+    // a username:password combination.
+    // Ignores also any query parameter because it may contain a http:// inside.
+    return (textUrl.replace(/\?.*/, '').indexOf('://') === -1 ? 'http://' : ''
+      ) + textUrl
+  }
+
+  /**
+  * Modfication du favicon en fonction du site auquel on se connecte
+  */
+  function setUrlFaviconAll(url) 
+  {
+    var FAVICON_BASE_URL = '//www.google.com/s2/favicons?domain_url='
+
+    $scope.urlFavicon = FAVICON_BASE_URL + url
+
+    faviconIsSet = true
+  }
+
+
 }
